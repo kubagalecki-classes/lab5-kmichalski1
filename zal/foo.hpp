43a3b9b53fd924d3436d2017b3b29a38127d83af
kmichalski1 <73621723+kmichalski1@users.noreply.github.com>
2020-12-09 23:52:35 +0100

    Solution

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -7,6 +7,23 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+  std::vector< char > wektor(people.size());
+  int i=0;
+  
+  for (std::list< Human >::iterator it = people.end(); it != people.begin();)
+  { 
+    --it;
+    it -> birthday();
+    if(it->isMonster()== true)
+    {
+      wektor[i]= 'n';  
+    }
+    else
+    {
+        wektor[i]= 'y';
+    }
+    i++;
+  }
+    
+  return wektor; 
 }
